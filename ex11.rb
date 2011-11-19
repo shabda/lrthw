00ccae4891afefc6363b72807640276a19e4ba23
Shabda Raaj <shabda@agiliq.com>
2011-11-19 21:34:57 +0530

    Q11

---
+++ ex11.rb
@@ -0,0 +1,10 @@
+print "What is your name?"
+name = gets.chomp()
+
+print "Was ist Ihr Name?"
+german_name = gets.chomp()
+
+print "Woher kommen Sie"
+you_come_from = gets.chomp()
+
+puts "Your name is #{german_name} and you come from #{you_come_from}"
