06c890514120ad3111fca1f1c06ac8908c7b949d
Shabda Raaj <shabda@agiliq.com>
2011-11-20 10:39:28 +0530

    Q19

---
+++ ex19.rb
@@ -0,0 +1,15 @@
+def buns_and_burgers(buns, burgers)
+  puts "you have #{buns} buns"
+  puts "you have #{burgers} burgers"
+  puts "Thats a lot"
+  puts 
+end
+
+puts "you have:"
+buns_and_burgers(20, 30)
+
+lots_of_burgers = 200
+lots_of_buns = 300
+buns_and_burgers(lots_of_buns, lots_of_burgers)
+
+buns_and_burgers(20*30, 300*200)
