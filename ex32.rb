294e1c72f7be26f6d93b624cbc581e697dac7679
Shabda Raaj <shabda@agiliq.com>
2011-11-20 20:07:12 +0530

    Q32

---
+++ ex32.rb
@@ -0,0 +1,17 @@
+counts = [1, 2, 3, 4, 5]
+fruits = ["Banana", "oranges", "Mango"]
+animals = ["Rohit", "Ash", "Saket", "Monu"]
+
+for i in counts
+  print i
+
+end
+puts
+
+for fruit in fruits
+  puts fruit
+end
+
+for animal in animals
+  puts animal
+end
